7bf6d82d71369301dd7fcff28ca1e59231464f6d
Anthony Poschen <zanven42@gmail.com>
2022-11-30 20:41:16 +1100

    Basic working service completed. Need to fix k8s config loading and packaging

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,14 +6,26 @@ import { TypeOf } from "zod";
 import { trpc, RouterOutputs } from "../utils/trpc";
 
 import { ReactNode } from 'react'
+import { useMutation } from "@tanstack/react-query";
 
 type serverStatus = "Unknown" | "Online" | "Shutting Down" | "Offline" | "Starting"
 const Home: NextPage = () => {
 const servers = trpc.kubernetes.get.useQuery();
+const TurnServerOnOff = trpc.kubernetes.set.useMutation()
+
+const ChangeServerState = async (server: RouterOutputs['kubernetes']['get'][number]) => {
+console.log("Sending HTTP request")
+TurnServerOnOff.mutate({
+Name: server.Name, Namespace: server.Namespace, Type: server.Type, Replicas:
+server.CurrentReplicas == 0 ? 1 : 0
+})
+};
+// custom border under heading
 const customStyle = {
-borderImage: "linear-gradient(to right, rgba(80,80,80,0), rgba(80,80,80,1),rgba(80,80,80,0)) 1;",
+borderImage: "linear-gradient(to right, rgba(80,80,80,0), rgba(80,80,80,1),rgba(80,80,80,0)) 1",
 }
 return (
+
 <>
 
   <Head>
@@ -21,31 +33,35 @@ return (
     <meta name="description" content="Game server dashboard" />
     <link rel="icon" href="/favicon.ico" />
   </Head>
-  <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+  <main className="flex min-h-screen flex-col items-center bg-gray-800 from-[#2e026d] to-[#15162c] text-slate-200">
     <div className="text-4xl text-center font-bold pt-5 w-full border-b-4 pb-2" style={customStyle}>
       Game Server Manager
     </div>
-    <div className="flex flex-wrap w-full h-full p-8 lg:p-2 lg:w-1/2 justify-center text-black">
-      {servers.data?.Servers.map((el) =>
-      <div className="w-full flex flex-grow p-2 even:bg-gray-400 odd:bg-gray-500 hover:bg-gray-300">
-        <h2 className="w-[150px] font-bold text-xl">
-          {el.Name}
+    <div className="flex flex-wrap w-full h-full p-8 lg:p-2 lg:w-1/2 justify-center">
+      {servers.data?.map((el) =>
+      <div className="w-full flex flex-grow p-2 even:bg-gray-500 odd:bg-gray-600">
+        <h2 className="w-[250px] font-bold text-xl">
+          {el.Namespace}/{el.Name}
         </h2>
         <p className="grow text-center">
           {CalculateStatus(el)}
         </p>
-        {["Online", "Offline"].includes(CalculateStatus(el)) ?
-        <button className="w-[150px]" type="button">Turn {CalculateStatus(el) == "Online" ? "Off" : "On"}</button> :
-        <div className="w-[150px]" />
-        }
+        {["Online", "Offline"].includes(CalculateStatus(el)) ? CalculateStatus(el) == "Online" ?
+        <button className="w-[150px] border-slate-500 bg-red-700 hover:bg-red-600 rounded" type="button" onClick={()=>
+          ChangeServerState(el)}>Turn Off</button>
+        :
+        <button className="w-[150px] border-slate-500 bg-green-700 hover:bg-green-600 rounded" type="button"
+          onClick={()=>
+          ChangeServerState(el)}>Turn On</button>
+        :
+        <div className="w-[150px]" />}
       </div>)}
     </div>
   </main>
 </>
 );
 };
-
-function CalculateStatus(server: RouterOutputs['kubernetes']['get']['Servers'][number]): serverStatus {
+function CalculateStatus(server: RouterOutputs['kubernetes']['get'][number]): serverStatus {
 var { DesiredReplicas, CurrentReplicas } = server
 if (CurrentReplicas == 1 && DesiredReplicas == 1) {
 return "Online"
--- src/server/trpc/router/kubernetes.ts
+++ src/server/trpc/router/kubernetes.ts
@@ -1,97 +1,88 @@
 import { z } from "zod";
 import { router, publicProcedure } from "../trpc";
-import k8s from '@kubernetes/client-node';
+import * as k8s from '@kubernetes/client-node';
 
 const kc: k8s.KubeConfig = new k8s.KubeConfig();
-kc.loadFromDefault();
+// kc.loadFromDefault();
+kc.loadFromFile('/Users/zanven/.kube/config')
 
-const k8sHPA = kc.makeApiClient(k8s.AutoscalingV2Api);
+const k8sContainer = kc.makeApiClient(k8s.AppsV1Api);
 // const k8scon = kc.makeApiClient(k8s.);
-const zServer = z.object({ Name: z.string(), Namespace: z.string(), HPAName: z.string(), DesiredReplicas: z.number(), CurrentReplicas: z.number() }).array()
+const zServerType = z.enum(["Deployment", "Statefulset"])
+const zServer = z.object({ Name: z.string(), Namespace: z.string(), Type: zServerType, DesiredReplicas: z.number(), CurrentReplicas: z.number() }).array()
 export const kubernetesRouter = router({
-  get: publicProcedure.output(z.object({ Servers: zServer })).query(() => {
-    // Call Golang microservice to get the API results,
-    // Map the return structure to a known type then return it down here
-    // var results 
-    // var results;
+  get: publicProcedure.output(zServer).query(async () => {
+    let deployPromise = k8sContainer.listDeploymentForAllNamespaces().then((data) => {
+      var results: typeof zServer._type = [];
+      data.body.items.forEach((item) => {
 
-    var results: typeof zServer._type = [];
-    k8sHPA.listHorizontalPodAutoscalerForAllNamespaces().then((data) => {
-      data?.body.items.forEach((item) => {
+        if (item.metadata?.namespace == undefined || item.metadata.name == undefined) {
+          return
+        }
         if (item.metadata?.namespace?.startsWith("games-") == false) {
           return
         }
-        if (item.spec == undefined || item.metadata == undefined || item.metadata.namespace == undefined || item.metadata.name == undefined || item.status == undefined || item.status.currentReplicas == undefined || item.status.desiredReplicas == undefined) {
+        if (item.status?.replicas == undefined || item.spec?.replicas == undefined) { return }
+        results.push({
+          Name: item.metadata.name, Namespace: item.metadata.namespace, CurrentReplicas: item.status?.readyReplicas || 0, DesiredReplicas: item.spec?.replicas, Type: "Deployment",
+        })
+      })
+      return results
+    }).catch((e) => {
+      console.log(e);
+      var results: typeof zServer._type = [];
+      return results;
+    })
+    let statefulsetPromise = k8sContainer.listStatefulSetForAllNamespaces().then((data) => {
+      var results: typeof zServer._type = [];
+      data.body.items.forEach((item) => {
+        if (item.metadata?.namespace == undefined || item.metadata.name == undefined) {
           return
         }
-        results.push(
-          {
-            Name: item.spec.scaleTargetRef.name,
-            Namespace: item.metadata.namespace,
-            HPAName: item.metadata?.name,
-            CurrentReplicas: item.status.currentReplicas,
-            DesiredReplicas: item.status.desiredReplicas,
-          })
+        if (item.metadata?.namespace?.startsWith("games-") == false) {
+          return
+        }
+        console.log(item)
+        if (item.status?.replicas == undefined || item.spec?.replicas == undefined) { return }
+        results.push({
+          Name: item.metadata.name, Namespace: item.metadata.namespace, CurrentReplicas: item.status?.readyReplicas || 0, DesiredReplicas: item.spec?.replicas, Type: "Statefulset",
+        })
       })
+      return results
+    }).catch((e) => {
+      console.log(e);
+      var results: typeof zServer._type = [];
+      return results;
     })
-    return results
-    //
-    // return {
-    //   Servers: [
-    //     {
-    //       Name: "factorio",
-    //       Namespace: "ns-1",
-    //       HPAName: "hpa-name",
-    //       DesiredReplicas: 1,
-    //       CurrentReplicas: 1,
-    //       HasHPA: true,
-    //     },
-    //     {
-    //       Name: "factorio 2",
-    //       Namespace: "ns-2",
-    //       HPAName: "hpa-name",
-    //       DesiredReplicas: 1,
-    //       CurrentReplicas: 1,
-    //       HasHPA: false,
-    //     },
-    //     {
-    //       Name: "game2",
-    //       Namespace: "ns-3",
-    //       HPAName: "hpa-name",
-    //       DesiredReplicas: 0,
-    //       CurrentReplicas: 0,
-    //       HasHPA: true,
-    //     },
-    //     {
-    //       Name: "game3",
-    //       Namespace: "ns-4",
-    //       HPAName: "hpa-name",
-    //       DesiredReplicas: 0,
-    //       CurrentReplicas: 1,
-    //       HasHPA: true,
-    //     },
-    //     {
-    //       Name: "game4",
-    //       Namespace: "ns-5",
-    //       HPAName: "hpa-name",
-    //       DesiredReplicas: 1,
-    //       CurrentReplicas: 0,
-    //       HasHPA: true,
-    //     },
-    // ],
-    // };
-  }),
 
-  set: publicProcedure.input(z.object({ HPAName: z.string(), Namespace: z.string(), Replicas: z.number() })).output(z.boolean()).mutation((q) => {
-    var { HPAName, Namespace, Replicas } = q.input
-    var returnValue = true
-    k8sHPA.readNamespacedHorizontalPodAutoscaler(HPAName, Namespace).then((value) => {
-      if (value?.body?.status?.desiredReplicas == undefined) {
-        return false
-      }
-      value.body.status.desiredReplicas = Replicas
-      k8sHPA.replaceNamespacedHorizontalPodAutoscaler(HPAName, Namespace, value.body).catch((e) => { console.log(e); returnValue = false })
-    }).catch(() => { returnValue = false })
-    return returnValue
+    return Promise.all([deployPromise, statefulsetPromise]).then((results) => {
+      var result: typeof zServer._type = [];
+      results.forEach((array) => {
+        result.push(...array)
+      })
+      return result
+    })
   }),
-});
+  set: publicProcedure.input(z.object({ Name: z.string(), Namespace: z.string(), Type: zServerType, Replicas: z.number() })).output(z.boolean()).mutation(async (q) => {
+    let { Name, Namespace, Type, Replicas } = q.input
+    console.log("Request recieved", q.input)
+    switch (Type) {
+      case "Deployment": {
+        return k8sContainer.readNamespacedDeployment(Name, Namespace).then((data) => {
+          let deployment = data.body;
+          if (deployment.spec?.replicas == undefined) { return false }
+          deployment.spec.replicas = Replicas
+          return k8sContainer.replaceNamespacedDeployment(Name, Namespace, deployment).then(() => { return true }).catch(() => { return false })
+        }).catch((e) => { console.log(e); return false })
+      }
+      case "Statefulset": {
+        return k8sContainer.readNamespacedStatefulSet(Name, Namespace).then((data) => {
+          let sts = data.body;
+          if (sts.spec?.replicas == undefined) { return false }
+          sts.spec.replicas = Replicas
+          return k8sContainer.replaceNamespacedStatefulSet(Name, Namespace, sts).then(() => { return true }).catch(() => { return false })
+        }).catch((e) => { console.log(e); return false })
+      }
+    }
+  })
+})

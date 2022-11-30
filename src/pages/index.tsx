2bcf8f8c5244eaf3792c9bbe39a6055aff95cc50
Anthony Poschen <zanven42@gmail.com>
2022-11-30 21:35:31 +1100

    made app more responsive for mobile usage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,24 +34,25 @@ return (
     <link rel="icon" href="/favicon.ico" />
   </Head>
   <main className="flex min-h-screen flex-col items-center bg-gray-800 from-[#2e026d] to-[#15162c] text-slate-200">
-    <div className="text-4xl text-center font-bold pt-5 w-full border-b-4 pb-2" style={customStyle}>
+    <div className="text-3xl md:text-4xl text-center font-bold pt-5 w-full border-b-4 pb-2" style={customStyle}>
       Game Server Manager
     </div>
-    <div className="flex flex-wrap w-full h-full p-8 lg:p-2 lg:w-1/2 justify-center">
+    <div className="flex flex-wrap w-full h-full p-0 pt-2 md:p-2 lg:p-2 lg:w-[900px] justify-center">
       {servers.data?.map((el) =>
       <div className="w-full flex flex-grow p-2 even:bg-gray-500 odd:bg-gray-600">
-        <h2 className="w-[250px] font-bold text-xl">
+        <h2 className="md:w-[250px] sm:w-auto md:font-bold md:text-xl">
           {el.Namespace}/{el.Name}
         </h2>
         <p className="grow text-center">
           {CalculateStatus(el)}
         </p>
         {["Online", "Offline"].includes(CalculateStatus(el)) ? CalculateStatus(el) == "Online" ?
-        <button className="w-[150px] border-slate-500 bg-red-700 hover:bg-red-600 rounded" type="button" onClick={()=>
+        <button className="w-[100px] md:w-[150px] border-slate-500 bg-red-700 hover:bg-red-600 rounded-md" type="button"
+          onClick={()=>
           ChangeServerState(el)}>Turn Off</button>
         :
-        <button className="w-[150px] border-slate-500 bg-green-700 hover:bg-green-600 rounded" type="button"
-          onClick={()=>
+        <button className="w-[100px] md:w-[150px] border-slate-500 bg-green-700 hover:bg-green-600 rounded-md"
+          type="button" onClick={()=>
           ChangeServerState(el)}>Turn On</button>
         :
         <div className="w-[150px]" />}
